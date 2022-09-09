e67325340fe0c12d5b35ade157b28dc7a4af691b
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-09-08 23:00:43 -0600

    update CORS

--- src/app.py
+++ src/app.py
@@ -1,10 +1,19 @@
 from __future__ import annotations
 
 import pydantic
-import xarray as xr
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ['*']
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 
 @app.get('/')
@@ -13,8 +22,19 @@ def index():
 
 
 @app.get('/xarray/')
-def xarray(dataset: pydantic.AnyHttpUrl = None):
+def xarray(
+    url: pydantic.AnyHttpUrl = Query(
+        ...,
+        description='URL to a zarr store',
+        example='https://ncsa.osn.xsede.org/Pangeo/pangeo-forge/HadISST-feedstock/hadisst.zarr',
+    )
+):
 
-    ds = xr.open_dataset(dataset, engine='zarr', chunks={})
+    import xarray as xr
 
-    return {'repr': ds._repr_html_().strip(), 'dataset': dataset}
+    with xr.open_dataset(url, engine='zarr', chunks={}) as ds:
+        html = ds._repr_html_().strip()
+
+    del ds
+
+    return {'html': html, 'dataset': url}

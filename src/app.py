22791e10a4beae1c49fb8e1ec015f86141fc709d
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-09-08 19:09:55 -0600

    update xarray route

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import pydantic
+import xarray as xr
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,3 +10,11 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {'message': 'Hello World!'}
+
+
+@app.get('/xarray/')
+def xarray(dataset: pydantic.AnyHttpUrl = None):
+
+    ds = xr.open_dataset(dataset, engine='zarr', chunks={})
+
+    return {'repr': ds._repr_html_().strip(), 'dataset': dataset}
